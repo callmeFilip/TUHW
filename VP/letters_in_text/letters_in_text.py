19d0f313eb84ef27866f8eca16aa0aeb8aa8d6bf
callmeFilip <filip_andonow@abv.bg>
2022-10-07 22:47:52 +0300

    Count letters in text

---
+++ VP/letters_in_text/letters_in_text.py
@@ -0,0 +1,22 @@
+from ast import Or
+
+
+FIRST_LETTER_CAPITAL = ord('A')
+FIRST_LETTER_LOWERCASE = ord('a')
+LAST_LETTER_CAPITAL = ord('Z')
+LAST_LETTER_LOWERCASE = ord('z')
+
+
+def count_letters(text):
+    counter = 0
+    for i in text:
+        numeric_representation = ord(i)
+        if (numeric_representation > FIRST_LETTER_CAPITAL and numeric_representation < LAST_LETTER_CAPITAL) or (numeric_representation > FIRST_LETTER_LOWERCASE and numeric_representation < LAST_LETTER_LOWERCASE):
+            counter += 1
+
+    return counter
+
+
+text = str(input())
+print(count_letters(text))
+
