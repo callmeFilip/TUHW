d5d75b7d9bddf7e0edfa6cd84f5ea73d234feb00
callmeFilip <filip_andonow@abv.bg>
2022-10-07 21:10:01 +0300

    Find PI algorithm

--- VP/PI/pi.py
+++ VP/PI/pi.py
@@ -2,7 +2,7 @@ def find_pi(term):
     pi = 3
     term = (term * 2) + 1
     for i in range(2, term, 2):
-        if((i/2)%2 == 0):
+        if (i/2)%2 == 0:
             pi -= 4/(i*(i+1)*(i+2))
         else:
             pi += 4/(i*(i+1)*(i+2))
