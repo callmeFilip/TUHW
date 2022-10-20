450e674ae4341bcf7149d6b32010cb606ad709a5
callmeFilip <filip_andonow@abv.bg>
2022-10-20 21:49:01 +0300

    Sort twice and find mix/max

---
+++ VP/double_sort_min_max/double_sort_min_max.py
@@ -0,0 +1,15 @@
+numbers = [1, 8, 27, 9, 31, 5, 17]
+
+numbers.sort()
+print(numbers)
+
+numbers.sort(reverse=True)
+print(numbers)
+
+letters = ['A', 'e', 'P', 'l', 'F']
+letters.sort(key=str.lower)
+
+print(letters)
+
+# chislov spisuk, sluchaini chisla, 5 broq
+# mejdu vsqka dvoika se vmukva sbora na prilejashtite elementi
